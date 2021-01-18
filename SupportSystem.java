1be7581d283967877bd3682bfe5376f8162f70e0
Roberto <robertoab28@gmail.com>
2021-01-18 10:15:34 +0100

    Ahora puedes escribir bye con espacios delante para salir de la ayuda

--- SupportSystem.java
+++ SupportSystem.java
@@ -38,7 +38,7 @@ public class SupportSystem
 
         while(!finished) {
             String input = reader.getInput();
-
+            input = input.trim();
             if(input.startsWith("bye")) {
                 finished = true;
             }
